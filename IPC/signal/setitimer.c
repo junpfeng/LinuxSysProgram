165371296561d0c1791a4bbfa75d783381328d82
xinyuefjp <867607989@qq.com>
2019-06-25 22:49:09 +0800

    完善了定时器信号

--- IPC/signal/setitimer.c
+++ IPC/signal/setitimer.c
@@ -3,8 +3,7 @@
 #include <sys/time.h>
 
 
-
-/*
+/* setitimer函数的定时参数类型结构体
  * struct itimerval  {
         struct timeval{
             it_value.tv_sec;
@@ -18,16 +17,21 @@
 
     } it, oldit;
 */
+
+// 封装了一个自定义的定时器
 unsigned int my_alarm(unsigned int sec)
 {
 	struct itimerval it, oldit;
     int ret;
-
+    // 定时值
 	it.it_value.tv_sec = sec;
 	it.it_value.tv_usec = 0;
-	it.it_interval.tv_sec = 0;
-	it.it_interval.tv_usec = 0;
 
+    // 定时间隔
+	it.it_interval.tv_sec = 0; // 设置为0表示一次定时吗？？是的
+	it.it_interval.tv_usec = 0; // 
+
+    // 第一个参数设置自然定时
 	ret = setitimer(ITIMER_REAL, &it, &oldit);
     if (ret == -1) {
         perror("setitimer");
@@ -39,7 +43,7 @@ unsigned int my_alarm(unsigned int sec)
 int main(void)
 {
 	int i;
-	my_alarm(1);  //alarm(sec);
+	my_alarm(1);  //alarm(sec); 默认动作是结束进程
 
 	for(i = 0; ; i++)
 		printf("%d\n", i);
--- IPC/signal/setitimer1.c
+++ IPC/signal/setitimer1.c
@@ -9,18 +9,20 @@ void myfunc(int signo)
     ////
     //
     //
-    raise(SIGALRM); 
+ //   raise(SIGALRM); 
 }
 
 int main(void)
 {
 	struct itimerval it, oldit;
-	signal(SIGALRM, myfunc);   //注册SIGALRM信号的捕捉处理函数。
+	signal(SIGALRM, myfunc);   //注册SIGALRM信号的捕捉处理函数。自定义信号捕捉为执行自定义函数
 
+    // 定时时间5s
 	it.it_value.tv_sec = 5;
 	it.it_value.tv_usec = 0;
 
-	it.it_interval.tv_sec = 3;
+    // 定时间隔1s，所谓周期模式，是指首次定时按照上面的，之后周期定时按照下面的
+	it.it_interval.tv_sec = 1;
 	it.it_interval.tv_usec = 0;
 
 	if(setitimer(ITIMER_REAL, &it, &oldit) == -1){
