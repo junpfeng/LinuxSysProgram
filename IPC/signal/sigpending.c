3c2d7782d34e7cfb1b191bff69cf861d8d632b69
xinyuefjp <867607989@qq.com>
2019-06-26 22:18:13 +0800

    完善了sigpending

--- IPC/signal/sigpending.c
+++ IPC/signal/sigpending.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+ * 由于屏蔽了所有信号，导致无法使用ctrl+c终止当前进程。
+ * 但是使用ctrl+c之后，第二位，会产生未决信号，因为ctrl+c就是产生的编号2的信号
+ * 通过查表可知
+ */
 void printset(sigset_t *ped)
 {
 	int i;
@@ -10,7 +15,7 @@ void printset(sigset_t *ped)
 		if((sigismember(ped, i) == 1)){
 			putchar('1');
 		} else {
-			putchar('0');
+			putchar('0'); // ped是未决信号集，0表示第i个信号不在未决信号集里
 		}
 	}
 	printf("\n");
@@ -18,7 +23,7 @@ void printset(sigset_t *ped)
 
 int main(void)
 {
-	sigset_t set, ped;
+	sigset_t set, ped; // 信号集
 #if 0
 	sigemptyset(&set);
 	sigaddset(&set, SIGINT);
@@ -26,8 +31,8 @@ int main(void)
 	sigaddset(&set, SIGKILL);
 	sigaddset(&set, SIGSEGV);
 #endif
-	sigfillset(&set);
-	sigprocmask(SIG_BLOCK, &set, NULL);	//不获取原屏蔽字
+	sigfillset(&set); // 将set各个位置一
+	sigprocmask(SIG_BLOCK, &set, NULL);	//不获取原屏蔽字，屏蔽所有信号（甚至将-9也屏蔽了）
 
 	while(1){
 		sigpending(&ped);       //获取未决信号集
